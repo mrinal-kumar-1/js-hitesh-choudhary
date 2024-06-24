24c63e365e2700f9c5394196b02730822303705b
mrinal-kumar-1 <mrinaluser22kumar@gmail.com>
2024-06-24 12:37:13 +0530

    numbers and Math

---
+++ 01_basics/numbers_&_maths.js
@@ -0,0 +1,20 @@
+// console.log("Happy Today, I will be learning about numbers in js");
+// let a = 230.9856;
+
+// console.log(a.toFixed(4));
+
+// console.log(a.toLocaleString())
+
+// console.log(a.toPrecision(5))
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math.random());
+// console.log(Math.random() + 1);
+
+// important formula
+
+const min = 2;
+const max = 3;
+
+console.log(Math.floor(Math.random() * (max - min + 1) ) + min)
---
+++ 01_basics/string.js
@@ -0,0 +1,10 @@
+let a = new String('Hi what are you doing?');
+// console.log(a);
+// a.trim()
+// console.log(a);
+
+console.log(a.slice(-10,-5))
+// console.log(a.substring(0,3));
+
+console.log(a.includes('what'));
+console.log(a.split('what'));
