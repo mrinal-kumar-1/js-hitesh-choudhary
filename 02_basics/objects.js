ce457c29d20abe7a4202555903664dca2acf8c12
mrinal-kumar-1 <mrinaluser22kumar@gmail.com>
2024-06-24 14:47:16 +0530

    Objects

---
+++ 02_basics/objects.js
@@ -0,0 +1,34 @@
+// let obj1 = {
+//     hey: "hello",
+//     "hey 3": "hello 3",
+//     3: 9
+// }
+
+ // console.log(obj1["hey"])
+
+// const newSymb = Symbol('symbol as a key')
+
+// const obj2 = {
+//     "first" : "how are you ?",
+//     "second" : "how do you do ?",
+//     [newSymb] : "happy"
+// }
+
+// console.log(obj2[newSymb])
+// console.log(newSymb)
+
+//  console.log(obj2)
+
+// Object.freeze(obj2);
+// obj2['first'] = 'new'
+
+// console.log(obj2)
+
+// obj2.g = function()
+// {
+//     console.log(`Hello user ${obj2.first}`)
+
+// }
+
+// console.log(obj2.g())
+
