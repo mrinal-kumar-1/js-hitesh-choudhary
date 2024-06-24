1db41c208091998a2e0577bed2275b06c72c85e2
mrinal-kumar-1 <mrinaluser22kumar@gmail.com>
2024-06-24 14:09:52 +0530

    array and methods

---
+++ 02_basics/arrays.js
@@ -0,0 +1,16 @@
+// const newarray = [ "a", 'b', 3, {hi : "hello"}]
+// console.log(newarray)
+// console.log(newarray.splice(0, 0, {hey : "inserted"}))
+// console.log(newarray)
+
+
+// console.log(Array.from("Hello Ji"))
+// console.log(Array.of(3,'t',3,5,9))
+
+const  a = ['k', 'd',3,3]
+// const b = [4, 2,9,9,0,7]
+// let combined = [...a, 'hi',...b]
+// console.log(combined)
+
+
+console.log(a.join(','))
