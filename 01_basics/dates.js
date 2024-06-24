ca482d9f27b207d50f6416ef161578754b294a99
mrinal-kumar-1 <mrinaluser22kumar@gmail.com>
2024-06-24 13:33:21 +0530

    date in js

---
+++ 01_basics/dates.js
@@ -0,0 +1,15 @@
+let first = new Date();
+// console.log(first.toString())
+// console.log(first.toLocaleString())
+// console.log(first.toDateString())
+// console.log(first.toISOString())
+
+// console.log(first.toJSON())
+// console.log(first.getDate())
+
+
+const second = Date.now()
+
+// console.log(second.toLocaleString())
+console.log(second)
+
