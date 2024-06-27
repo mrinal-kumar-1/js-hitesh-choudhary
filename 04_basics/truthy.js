2222e5b3b09744635b6ffb1b2b7cd605abaa08ed
mrinal-kumar-1 <mrinaluser22kumar@gmail.com>
2024-06-27 12:38:42 +0530

    Truthy and falsy values

---
+++ 04_basics/truthy.js
@@ -0,0 +1,24 @@
+// let anArray = []
+
+// if(anArray.length === 0)
+//     {
+//         console.log("Array is empty");
+//     }
+
+
+// let anObject = {hi: "hello", 
+//                 milo: "Silo"
+// }
+
+// console.log(Object.entries(anObject))
+
+// if(Object.keys(anObject).length===0)
+//     {
+//         console.log("The object is empty")
+//     }
+
+//nullish coalescing operator
+
+let value = null ?? 56;
+
+console.log(value);
