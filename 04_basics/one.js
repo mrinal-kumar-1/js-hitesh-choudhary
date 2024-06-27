926ac319e638f706304b651c2bb6f288140242ef
mrinal-kumar-1 <mrinaluser22kumar@gmail.com>
2024-06-27 12:38:15 +0530

    control statements

---
+++ 04_basics/one.js
@@ -0,0 +1,20 @@
+//control flow
+
+if(2!=3)
+{
+    console.log('Hey I will be definitely printed');
+}
+
+let a = 60;
+
+if(a >20 && a<50)
+    {
+        console.log("Between 20 and 50");
+    }
+    else if(a > 50 && a < 80 )
+        {
+            console.log("Between 50 and 80");
+        }
+        else{
+            console.log("Greater than 80");
+        }
