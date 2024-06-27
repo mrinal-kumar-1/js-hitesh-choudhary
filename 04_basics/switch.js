6dc919995c546ff0abb99f7f9557ca05160ecf01
mrinal-kumar-1 <mrinaluser22kumar@gmail.com>
2024-06-27 12:38:27 +0530

    Switch Case

---
+++ 04_basics/switch.js
@@ -0,0 +1,15 @@
+let tv = "LG"
+
+switch(tv){
+    case "Samsung":
+        console.log("You are using Samsung TV")
+    case "Xiaomi":
+        console.log("You are using Xiaomi TV")
+    case "Realme":
+        console.log("You are using Realme TV")
+        // break;
+    case "LG":
+        console.log("You are using LG TV")
+    default:
+        console.log("default case")    
+}
