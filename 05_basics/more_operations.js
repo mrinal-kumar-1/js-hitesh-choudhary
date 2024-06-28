a2e4b4852f0e2eb5b482770e06d9f116148b538e
mrinal-kumar-1 <mrinaluser22kumar@gmail.com>
2024-06-28 12:00:36 +0530

    more fuctions for array

---
+++ 05_basics/more_operations.js
@@ -0,0 +1,43 @@
+// let books = [
+//     {
+//         name : "book1", genre : "History", publish : 1999
+//     },
+//     {
+//         name : "book2", genre : "Science", publish : 1989
+//     },
+//     {
+//         name : "book3", genre : "Non-Fiction", publish : 2001
+//     },
+//     {
+//         name : "book4", genre : "History", publish : 2005
+//     },
+//     {
+//         name : "book5", genre : "Fiction", publish : 1998
+//     }
+// ]
+
+// let values = books.filter( (item) => {
+//     return item.publish > 2000
+// })
+
+// console.log(values)
+
+let Numbers = [ 25, 35,35,36, 977, 83];
+
+// Numbers.forEach( (value) => {
+//     console.log(Numbers)
+// })
+
+// console.log(Numbers)
+
+// let newNumbers = Numbers.map( (values) => {
+//     return values + 1
+// })
+
+// console.log(newNumbers);
+
+//chaining of methods
+
+// let chained = Numbers.map( (num) => (num*9)).map((num) => (num + 2)).filter( (num) => (num > 500));
+
+// console.log(chained)
