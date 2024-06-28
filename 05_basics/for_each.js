c2354a3a3996965a181756fa86f3d189d9bea3c2
mrinal-kumar-1 <mrinaluser22kumar@gmail.com>
2024-06-28 11:09:30 +0530

    for each loop

---
+++ 05_basics/for_each.js
@@ -0,0 +1,41 @@
+// let trialArray = ["hello","hi","useful","user","define"]
+
+// trialArray.forEach(function (value){
+//     console.log(value)
+// })
+
+// trialArray.forEach((value) => {
+//     console.log(value)
+// })
+
+// trialArray.forEach(passing)
+
+// function passing( value)
+// {
+//   console.log(value)
+// }
+
+// trialArray.forEach((value, index, array) => (
+//     console.log(value, index, array )
+// ))
+
+// const myObject = [ 
+//     {
+//         studentName : "A",
+//         studentID : 33
+//     },
+//     {
+//         studentName : "B",
+//         studentID : 34
+//     },
+//     {
+//         studentName : "C",
+//         studentID : 35
+//     }
+// ]
+
+// myObject.forEach( (item) => {
+//     console.log(`Student's name is ${item.studentName}`);
+// })
+
+
